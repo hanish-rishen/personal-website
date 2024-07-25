76464cbd5cd9e177d9f586b3e8e4acd3c4483ec7
Hanish Rishen <hanishrishen@gmail.com>
2024-07-26 01:03:17 +0530

    many changes

--- app/experience/page.tsx
+++ app/experience/page.tsx
@@ -22,7 +22,7 @@ export default function Experience({ searchParams: { customUsername } }) {
 		<div className="bg-gradient-to-tl from-zinc-900/0 via-zinc-900 to-zinc-900/0">
 			<Navigation />
 			<div className="container flex items-center justify-center min-h-screen px-4 mx-auto">
-				<div className="grid w-full grid-cols-1 gap-8 mx-auto mt-32 sm:mt-32 sm:grid-cols-3 lg:gap-16">
+				<div className="grid w-full grid-cols-1 gap-8 mx-auto mt-32 mb-10 sm:mt-32 sm:grid-cols-3 lg:gap-16">
 					{experiences.map((exp) => (
 						<Card key={exp.title}>
 							<div className="p-4 flex flex-col items-center gap-4">
